b0fa7b0b0f7f82c5dab0a6e6b4fcbbf73e42c8ba
surprise4u <168637092+surprise4u@users.noreply.github.com>
2024-05-18 17:11:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Love you too din!>.<";
+  question.innerHTML = "Love you too!>.<";
   gif.src ="  ";
 });
 
